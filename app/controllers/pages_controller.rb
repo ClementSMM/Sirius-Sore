28bed9cd19f2d3ef01278bbb009e3364b0d2cea5
louise longle <louise.longle@gmail.com>
2021-04-27 19:27:45 +0200

    include banner of the awesome dog

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @gallery_banner = Gallery.find_by(category: "Bannière")
 
   end
 
@@ -10,14 +11,17 @@ class PagesController < ApplicationController
   end
 
   def about
+    @gallery_banner = Gallery.find_by(category: "Bannière")
 
   end
 
   def contact
+    @gallery_banner = Gallery.find_by(category: "Bannière")
 
   end
 
   def salon 
+    @gallery_banner = Gallery.find_by(category: "Bannière")
 
   end
 
