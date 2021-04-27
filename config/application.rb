8cdba28adb6133f69e261882a2c6b84a46c474cf
louise longle <louise.longle@gmail.com>
2021-04-27 17:31:50 +0200

    host heroku accepted

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module SiriusStore
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     config.hosts << '28e0235e4a15.ngrok.io'
+    config.hosts << "sirius-online-store.herokuapp.com"
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
