dca76b3af0193454c0f72beecdc0833d823b4eda
louise longle <louise.longle@gmail.com>
2021-04-30 16:44:25 +0200

    default images and fixed show order for non-signed-in

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,13 +13,12 @@ class Product < ApplicationRecord
 
   def find_first_price
     @product_prices = []
-    default_price = 6666
-    @product_prices << default_price
     self.stocks.each do |stock|
       @product_prices << stock.price_cents
     end
-    return @product_prices.sort!.first / 100
+    return @product_prices.sort!.first.to_f / 100
   end
+
 
   pg_search_scope :global_search,
     against: [ :name, :long_description, :small_description ],
